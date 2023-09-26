0a776c062a11766796dcbb7eb80a8f30ed485525
7akub <7akub@Jakubs-MacBook-Pro.local>
2023-09-26 13:13:54 +0200

    apply changes

--- android/src/main/kotlin/com/example/idenfy_sdk_flutter/IdenfySdkFlutterPlugin.kt
+++ android/src/main/kotlin/com/example/idenfy_sdk_flutter/IdenfySdkFlutterPlugin.kt
@@ -17,81 +17,99 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
+import idenfyviews
 
 /** IdenfySdkFlutterPlugin */
-class IdenfySdkFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-  private lateinit var activity: Activity
-  //Is nullable after proccess death.
-  private var mResult: Result?=null
+class IdenfySdkFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
+    PluginRegistry.ActivityResultListener {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private lateinit var activity: Activity
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "idenfy_sdk_flutter")
-    channel.setMethodCallHandler(this)
-  }
+    //Is nullable after proccess death.
+    private var mResult: Result? = null
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else if (call.method == "start") {
-      mResult = result
-
-      IdenfyCommonColors.idenfyMainColorV2 = UIColor.green
-      IdenfyCommonColors.idenfyMainDarkerColorV2 = UIColor.green
-
-      val idenfySettingsV2 = IdenfySettingsV2.IdenfyBuilderV2()
-              .withAuthToken(call.argument<String>("authToken")!!)
-              .build();
-
-      IdenfyController.getInstance().initializeIdenfySDKV2WithManual(this.activity, IdenfyController.IDENFY_REQUEST_CODE, idenfySettingsV2)
-    } else if (call.method == "startFaceAuth") {
-      mResult = result
-      val faceAuthenticationInitialization = FaceAuthenticationInitialization(call.argument<String>("token")!!, call.argument<Boolean>("withImmediateRedirect")!!)
-      IdenfyController.getInstance().initializeFaceAuthenticationSDKV2(this.activity, IdenfyController.IDENFY_REQUEST_CODE, faceAuthenticationInitialization)
-    } else {
-      //result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "idenfy_sdk_flutter")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method == "start") {
+            mResult = result
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    this.activity = binding.activity
-    binding.addActivityResultListener(this)
-  }
+            IdenfyCommonColors.idenfyMainColorV2 = UIColor.green
+            IdenfyCommonColors.idenfyMainDarkerColorV2 = UIColor.green
 
-  override fun onDetachedFromActivityForConfigChanges() {
-  }
+            val idenfySettingsV2 = IdenfySettingsV2.IdenfyBuilderV2()
+                .withAuthToken(call.argument<String>("authToken")!!)
+                .withIdenfyToolbarHidden()
+                .build();
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-  }
-
-  override fun onDetachedFromActivity() {
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
-    if (requestCode == IdenfyController.IDENFY_REQUEST_CODE) {
-      when (resultCode) {
-        IdenfyController.IDENFY_IDENTIFICATION_RESULT_CODE -> {
-          val idenfyIdentificationResult: IdenfyIdentificationResult = data!!.getParcelableExtra(IdenfyController.IDENFY_IDENTIFICATION_RESULT)!!
-          val jsonString = Gson().toJson(idenfyIdentificationResult)
-          mResult?.success(jsonString)
+            IdenfyController.getInstance().initializeIdenfySDKV2WithManual(
+                this.activity,
+                IdenfyController.IDENFY_REQUEST_CODE,
+                idenfySettingsV2
+            )
+        } else if (call.method == "startFaceAuth") {
+            mResult = result
+            val faceAuthenticationInitialization = FaceAuthenticationInitialization(
+                call.argument<String>("token")!!,
+                call.argument<Boolean>("withImmediateRedirect")!!
+            )
+            IdenfyController.getInstance().initializeFaceAuthenticationSDKV2(
+                this.activity,
+                IdenfyController.IDENFY_REQUEST_CODE,
+                faceAuthenticationInitialization
+            )
+        } else {
+            //result.notImplemented()
         }
-        IdenfyController.IDENFY_FACE_AUTHENTICATION_RESULT_CODE -> {
-          val faceAuthenticationResult: FaceAuthenticationResult = data!!.getParcelableExtra(IdenfyController.IDENFY_FACE_AUTHENTICATION_RESULT)!!
-          val jsonString = Gson().toJson(faceAuthenticationResult)
-          mResult?.success(jsonString)
-        }
-      }
-    } else {
-      //mResult?.notImplemented()
     }
-    return true
-  }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        this.activity = binding.activity
+        binding.addActivityResultListener(this)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    }
+
+    override fun onDetachedFromActivity() {
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+        if (requestCode == IdenfyController.IDENFY_REQUEST_CODE) {
+            when (resultCode) {
+                IdenfyController.IDENFY_IDENTIFICATION_RESULT_CODE -> {
+                    val idenfyIdentificationResult: IdenfyIdentificationResult =
+                        data!!.getParcelableExtra(IdenfyController.IDENFY_IDENTIFICATION_RESULT)!!
+                    val jsonString = Gson().toJson(idenfyIdentificationResult)
+                    mResult?.success(jsonString)
+                }
+
+                IdenfyController.IDENFY_FACE_AUTHENTICATION_RESULT_CODE -> {
+                    val faceAuthenticationResult: FaceAuthenticationResult =
+                        data!!.getParcelableExtra(IdenfyController.IDENFY_FACE_AUTHENTICATION_RESULT)!!
+                    val jsonString = Gson().toJson(faceAuthenticationResult)
+                    mResult?.success(jsonString)
+                }
+            }
+        } else {
+            //mResult?.notImplemented()
+        }
+        return true
+    }
 }
